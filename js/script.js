6a741e71a2abdf3d158bb9d207b24e9866517eb0
danielemurgioni <dashomew@gmail.com>
2025-03-29 12:35:09 +0100

    [UPD] use FOR to cicle array shop and get the weight of the objects inside it

--- js/script.js
+++ js/script.js
@@ -15,6 +15,12 @@ const shop = [
         name: "Bici yatta",
         weight: 80
     }
-]
+];
+
+// Stampa la bici con il peso minore: 
+// ciclo l'array shop con FOR e vado a prendere il peso degli oggetti Bici
+for(let i=0; i<shop.length; i++){
+    console.log(shop[i].weight);
+}
 
 console.log(shop)
