79d024cda0791c1df601cd65bbfbdd55231d0fb8
danielemurgioni <dashomew@gmail.com>
2025-03-29 12:27:06 +0100

    [UPD] Created an array of objects

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const shop = [
+    {
+        name: "Bici beta",
+        weight: 10 
+    },
+    {
+        name: "Bici delta",
+        weight: 20
+    },
+    {
+        name: "Bici gamma",
+        weight: 40
+    },
+    {
+        name: "Bici yatta",
+        weight: 80
+    }
+]
+
+console.log(shop)
