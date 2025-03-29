458e5b5369aab205696a9686563a8a0b8e77d7dc
danielemurgioni <dashomew@gmail.com>
2025-03-29 13:10:17 +0100

    [UPD] determinated the first element of the array and check if the other elements are minor or equal to it

--- js/script.js
+++ js/script.js
@@ -17,10 +17,20 @@ const shop = [
     }
 ];
 
-// Stampa la bici con il peso minore: 
-// ciclo l'array shop con FOR e vado a prendere il peso degli oggetti Bici
+// creo una variabile dove prendo il peso del primo elemento dell'array
+let minWeight = shop[0].weight;
+//creo una variabile che rappresenterà il risultato che voglia ottenere (la bici con il peso minore)
+let lessHeavy;
+
+// ciclo l'array shop con FOR, quindi controllo le bici
 for(let i=0; i<shop.length; i++){
-    console.log(shop[i].weight);
+    // se il peso della bici in posizione [i] è minore o uguale di minWeight allora assegno il valore a lessHeavy
+
+    //(solo con minore non riesco a determinare il peso minore nel caso io abbia il valore minore al primo posto nell'array)[quindi mi risulterebbe "undefined"]
+
+    if(shop[i].weight <= minWeight){
+        lessHeavy = shop[i];
+    }
 }
 
-console.log(shop)
+console.log(lessHeavy);
